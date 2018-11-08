fe32e6769bbd653f6f3850bb2b26488082e76f5b
IplayLF2 <914637118@qq.com>
2018-11-08 18:27:21 +0800

    写了环境引用部分内容。

--- Javascript的环境模型/代码片段.js
+++ Javascript的环境模型/代码片段.js
@@ -15,3 +15,13 @@ function main() {   //1
     addX(value);    //8
 }   //9
 main(); //10
+
+var getCounter = function (start) { //1
+    return function () {    //2
+        return start++; //3
+    };  //4
+};  //5
+var counter = getCounter(0);    //6
+counter();  //7
+counter();  //8
+counter();  //9
