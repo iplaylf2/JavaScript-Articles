5011cea1741f714b627eb628570acac125651026
IplayLF2 <914637118@qq.com>
2019-03-08 10:35:59 +0800

    整理demo的格式

--- Javascript的环境模型/模拟环境模型的demo.js
+++ Javascript的环境模型/模拟环境模型的demo.js
@@ -12,24 +12,18 @@
 Environment.Global.defineVariable('getCounter');
 Environment.Global.setVariable(
     'getCounter',
-    Environment.Global.defineFunction(
-        new $Function(
-            function ($) {
-                return $.defineFunction(
-                    new $Function(
-                        function ($) {
-                            $.defineVariable('result');
-                            $.setVariable('result', $.getVariable('start'));
-                            $.setVariable('start', $.getVariable('start') + 1);
-                            return $.getVariable('result');
-                        },
-                        { functionName: 'counter' }
-                    )
-                );
-            },
-            { parameterList: ['start'], functionName: 'getCounter' }
-        )
-    )
+    Environment.Global.defineFunction(new $Function(
+        function ($) {
+            return $.defineFunction(new $Function(
+                function ($) {
+                    $.defineVariable('result');
+                    $.setVariable('result', $.getVariable('start'));
+                    $.setVariable('start', $.getVariable('start') + 1);
+                    return $.getVariable('result');
+                },
+                { functionName: 'counter' }));
+        },
+        { parameterList: ['start'], functionName: 'getCounter' }))
 );
 Environment.Global.defineVariable('counter');
 Environment.Global.setVariable('counter', getCounter(0));
